d1c8175d61d013ad056fa52fb996dedc794abb1b
SebastianTong <tongsebastian27@gmail.com>
2023-03-10 18:13:16 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react'
+import ParticlesBg from 'particles-bg'
+import Navigation from './components/Navigation/Navigation'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Logo from './components/Logo/Logo'
+import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import SignIn from './components/SignIn/SignIn'
+import Register from './components/Register/Register'
+import './App.css';
+
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: '',
+    joined: ''
+  }
+}
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = initialState
+  }
+
+  loadUser = (data) => {
+    this.setState({ 
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return { 
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+     }
+  } 
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onPictureSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+         this.setState(Object.assign(this.state.user , { entries: count }))
+        })
+        .catch(err => {
+          console.log(err, 'error');
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(error => console.log(error, 'error'))
+  }
+
+  onRouteChange = (passedRoute) => {
+    if (passedRoute === 'signout') {
+      this.setState(initialState)
+    } else if (passedRoute === 'home'){
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ route: passedRoute})
+  };
+
+  render(){
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return(
+    <div className="App">
+      <ParticlesBg type='circles' bg={true} color='#FFFFFF'/>
+      <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
+      {route === 'home'
+        ? <div>
+            <Logo />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+            <ImageLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit}/>
+            <FaceRecognition imageUrl={imageUrl} box={box}/>
+            </div>
+        :(
+          route === 'signin'
+          ?<SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
+          :<Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
+        )
+      }
+    </div>
+    );
+  }
+}
+
+export default App;
