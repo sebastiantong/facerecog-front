d76341ccd15f7dd04b370a6b39962d8f0c90962c
SebastianTong <tongsebastian27@gmail.com>
2023-03-11 17:36:07 -0300

    changed url for requests

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -15,7 +15,7 @@ class SignIn extends React.Component {
     this.setState({signInPassword: event.target.value})
   }
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://facerecog-api-42ip.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
